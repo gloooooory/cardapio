dbac62fbd77501681c62dc18fe338b951c4b35a7
gloooooory <glo8ory@gmail.com>
2024-02-08 11:54:45 -0300

    feat: adds Post method map and adds CrossOrigin anothation

--- src/main/java/com/example/cardapio/controller/FoodController.java
+++ src/main/java/com/example/cardapio/controller/FoodController.java
@@ -1,12 +1,9 @@
 package com.example.cardapio.controller;
 
-import com.example.cardapio.food.Food;
-import com.example.cardapio.food.FoodRepository;
+import com.example.cardapio.food.*;
 import com.example.cardapio.food.FoodRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,10 +13,20 @@ public class FoodController {
 
     @Autowired
     private FoodRepository repository;
-    @GetMapping
-   public List<Food> getAll(){
 
-        List<Food> foodList = repository.findAll();
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public void saveFood(@RequestBody FoodRequestDTO data) {
+        Food foodData = new Food(data);
+        repository.save(foodData);
+        return;
+    }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @GetMapping
+   public List<FoodResponseDTO> getAll(){
+
+        List<FoodResponseDTO> foodList = repository.findAll().stream().map(FoodResponseDTO::new).toList();
         return foodList;
     }
 }
