8f89052710f2fc034bccce394371e21fab69545b
gloooooory <glo8ory@gmail.com>
2024-02-15 16:05:53 -0300

    feat: adds hooks

---
+++ src/hooks/useFoodData.ts
@@ -0,0 +1,25 @@
+import axios, { AxiosPromise } from "axios";
+import { FoodData } from "../interface/FoodData";
+import { useQuery } from "@tanstack/react-query";
+
+const API_URL = 'http://localhost:8080';
+
+const fetchData = async (): AxiosPromise<FoodData[]> => {
+  const response = axios.get(API_URL + '/food');
+  return response;
+}
+
+
+export function useFoodData() {
+  const query = useQuery({
+    queryFn: fetchData,
+    queryKey: ['food-data'],
+    retry: 2,
+  })
+
+  return { ...query,
+    data: query.data?.data
+   }
+
+
+}
